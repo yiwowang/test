fbed70e9843e570bd135a4f52f140008fb948d50
sunguowei <sunguowei@baidu.com>
2017-03-10 15:05:30 +0800

    ddd

--- app/src/main/java/com/example/sunguowei/testgit/MainActivity.java
+++ app/src/main/java/com/example/sunguowei/testgit/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    
+    public String name = "我是新手，提交了本行代码";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
