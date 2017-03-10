7e413bf1ee7fee334f92d08973d785db889cfd1f
sunguowei <sunguowei@baidu.com>
2017-03-10 16:51:03 +0800

    111

--- app/src/main/java/com/example/sunguowei/testgit/MainActivity.java
+++ app/src/main/java/com/example/sunguowei/testgit/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // from bugfix branch
+        // from bugfix branch2
     }
 }
